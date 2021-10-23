8df56111f6adc8d35a22bdf579dadbb683d75aae
Salvatore <sparascandola1@gmail.com>
2021-10-22 23:52:47 -0700

    binary2Hex GOOD

--- Binary_Conversion.c
+++ Binary_Conversion.c
@@ -2,51 +2,65 @@
 #include <stdlib.h>
 #include <string.h>
 
-// turn binary to hexadecimal
-void binary2Hex(int input, char output[])
-{   
-    //local vars
-    char digit[2] = {'0', '\0'};
-    int nibble = strlen(output);
+// Function Prototypes
+void decimal2Binary(int inut, char output[]);
+int binary2Decimal(char input[]);
+void binary2Hex(int input, char output[]);
 
-    // while nibble is within array bounds build Hexadecimal string
-    while (nibble > 0)
-    {
-        sprintf(digit, "0x0F", input);
-        output[nibble - 1] = digit[0];
-
-        nibble = nibble - 1;
-
-        input = input >> 4;
-    }
-}
-
-// binary to decimal
-int binary2Decimal(char input[])
+int main()
 {
-    // local vars
-    int output = 0;
-    int power = 1;
+    // decimal number
+    int input = 29;
 
-    // loop over the binary code and calculate the decimal
-    for (int i = strlen(input) - 1; i >= 0; i--)
+    // array of emtpy bits
+    char output1[] = {'0', '0', '0', '0', '0', '0', '0', '0', '\0'};
+    char output2[3] = {'0', '0', '\0'};
+
+    // run logic for decimal into binary
+    decimal2Binary(input, output1);
+
+    // run logic for binary into decimal
+    int result = binary2Decimal(output1);
+
+    // print input
+    printf("Input: %d in binary: ", input);
+
+    // get size of array for loop
+    size_t length = strlen(output1);
+    // print the binary code for the decimal
+    for (int i = 0; i < length; i++)
     {
-        output = output + ((input[i] - 48) * power);
-
-        power = power * 2;
+        printf("%c", output1[i]);
     }
 
-    // return decimal
-    return output;
+    // format
+    printf("\n");
+
+    // print result
+    printf("Result for binary to decimal: %d\n", result);
+
+    // run logic for binary to hex
+    binary2Hex(input, output2);
+
+    // get size of array for loop
+    length = strlen(output2);
+    // print the binary code for the decimal
+    for (int i = 0; i < length; i++)
+    {
+        printf("%c", output2[i]);
+    }
+
+    // format
+    printf("\n");
+
+    // exit clean
+    exit(EXIT_SUCCESS);
 }
 
 void decimal2Binary(int input, char output[])
 {
-    //quotient will be the decimal number to convert
-    int quotient = -1;
-
-    // make bit the size of the array -1
-    int bit = strlen(output) - 1;
+    //quotient will be the decimal number to convert make bit the size of the array -1
+    int quotient = -1, bit = strlen(output) - 1;
 
     // divide the decimal number by two and place the the remainder in the output array to build the binary number
     while (quotient != 0)
@@ -75,54 +89,44 @@ void decimal2Binary(int input, char output[])
     }
 }
 
-int main()
+// binary to decimal
+int binary2Decimal(char input[])
 {
-    // decimal number
-    int input = 29;
+    // local vars
+    int output = 0, power = 1;
 
-    // array of emtpy bits
-    char output1[] = {'0', '0', '0', '0', '0', '0', '0', '0', '\0'};
-    char output2[3] = {'0', '0', '\0'};
-
-    // get size of array for loop
-    size_t length = strlen(output1);
-
-    // run logic and transfor a decimal into binary
-    decimal2Binary(input, output1);
-
-    int result = binary2Decimal(output1);
-
-    // print input
-    printf("Input is: %d\n", input);
-
-    printf("%d in binary: ", input);
-
-    // print the binary code for the decimal
-    for (int i = 0; i < length; i++)
+    // loop over the binary code and calculate the decimal
+    for (int i = strlen(input) - 1; i >= 0; i--)
     {
-        printf("%c", output1[i]);
+        output = output + ((input[i] - 48) * power);
+
+        power = power * 2;
     }
 
-    // format
-    printf("\n");
+    // return decimal
+    return output;
+}
 
-    printf("Result for binary to decimal: %d\n", result);
+// turn binary to hexadecimal
+void binary2Hex(int input, char output[])
+{
+    //local vars
+    char digit[2] = "0\0";
+    int nibble = strlen(output);
 
-    binary2Hex(input, output2);
-
-    length = strlen(output2);
-
-    printf("Input is: %d\n", input);
-
-    // print the binary code for the decimal
-    for (int i = 0; i < length; i++)
+    // while nibble is within array bounds build Hexadecimal string
+    while (nibble > 0)
     {
-        printf("%c", output2[i]);
+        // mask the binary as hex
+        sprintf(&digit[0], "%1x", input & 0x0f);
+
+        // place hex in output array
+        output[nibble - 1] = digit[0];
+
+        // decrement the nibble
+        nibble = nibble - 1;
+
+        // shift the input bit 4 places
+        input = input >> 4;
     }
-
-    // format
-    printf("\n");
-
-    // exit clean
-    exit(EXIT_SUCCESS);
 }
